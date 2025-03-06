d98b460e4d415c6496b0f893a258775369180051
Mahmoud Mortada <mahmoud.mortada@AlAhlyMomkn.com>
2025-03-06 17:07:51 +0200

    fix base name

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ghPages from 'vite-plugin-gh-pages';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: "/my-portfolio",
+  plugins: [react(), ghPages()],
+  base: '/my-portfolio/', // Must match your GitHub Pages URL path
 });
